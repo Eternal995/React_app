3f1b978d778f0983a9f9ef930c22bf459c623d85
Yongda Long <ylong9@myseneca.ca>
2023-08-05 18:57:02 -0400

    Update Assignment 6

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 /*********************************************************************************
- *  WEB422 – Assignment 5
+ *  WEB422 – Assignment 6
  *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
  *  Name: Yongda Long Student ID: 172800211 Date: July 17, 2023
+ * 
+ *  Vercel App (Deployed) Link: https://react-app-sjx3.vercel.app/
  *
  ********************************************************************************/
 
